8cb14d084b0b0d8edd66aa87b525f83a51591cb4
Ivan Yanovich <s28218@pjwstk.edu.pl>
2025-03-03 09:46:16 +0100

    Initial project

--- Task2/Task2/Program.cs
+++ Task2/Task2/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hll, Word!");
+
+double GetAverage(int[] numbers) {
+    return 0;
+}
+
+int[] arr = {1,2,3,4,5};
+
+GetAverage(arr);
